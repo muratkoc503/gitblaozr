8a7c879f75c40f6b1c459b2896a43e61133149b8
Murat <muratkoc503@gmail.com>
2025-05-10 14:12:02 +0300

    Initial commit

---
+++ wwwroot/js/pdfGenerator.js
@@ -0,0 +1,14 @@
+﻿window.generatePdf = function (elementId, filename) {
+    const element = document.getElementById(elementId);
+
+    const opt = {
+        margin: 10,
+        filename: filename,
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    };
+
+    // html2pdf.js kullanarak PDF oluştur
+    html2pdf().from(element).set(opt).save();
+}
